d6d35a5bfb518fca0ed8c77c04379182246dcce4
m.sakamaki <mail@m-sakamaki.jp>
2016-04-07 20:45:53 +0900

    remove navigater polyfill

--- src/polyfill/mozI2c/mozI2c.js
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore next */
-if (!navigator.mozI2c) {
-  navigator.mozI2c = new Object();
-  navigator.mozI2c.open = () => void 0;
-  navigator.mozI2c.setDeviceAddress = () => void 0;
-  navigator.mozI2c.read = () => Promise.resolve(1);
-  navigator.mozI2c.write = () => Promise.resolve(void 0);
-}
